c80136ce60014d9fbb1c4cadf8d5502199fe57a4
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-09-07 22:08:37 +0600

    Update BalancedParentheses.java

--- 01_Data_Structures/02_Balanced Parentheses/BalancedParentheses.java
+++ 01_Data_Structures/02_Balanced Parentheses/BalancedParentheses.java
@@ -1,1 +1,40 @@
+import java.util.Stack;
 
+public class BalancedParentheses {
+    public static boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input1 = "((()))";
+        String input2 = "(()))";
+
+        boolean isBalanced1 = isBalanced(input1);
+        boolean isBalanced2 = isBalanced(input2);
+
+        System.out.println("Input: " + input1 + ", Balanced: " + isBalanced1);
+        System.out.println("Input: " + input2 + ", Balanced: " + isBalanced2);
+    }
+}
