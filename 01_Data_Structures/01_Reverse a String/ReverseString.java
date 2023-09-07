bf16e1bed49a56d62bfabe0c04c6cdb22409a2c1
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-09-07 22:02:18 +0600

    Update ReverseString.java

--- 01_Data_Structures/01_Reverse a String/ReverseString.java
+++ 01_Data_Structures/01_Reverse a String/ReverseString.java
@@ -1,1 +1,15 @@
+public class ReverseString {
+    public static String reverse(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
 
+    public static void main(String[] args) {
+        String input = "hello";
+        String reversedString = reverse(input);
+        System.out.println(reversedString);
+    }
+}
