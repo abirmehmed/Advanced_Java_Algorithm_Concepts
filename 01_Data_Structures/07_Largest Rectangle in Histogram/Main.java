ce0a07e4105dae203d5d9c554f17316f886d37e0
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-09-13 23:24:56 +0600

    Create Main.java

---
+++ 01_Data_Structures/07_Largest Rectangle in Histogram/Main.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class Main {
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int i = 0;
+
+        while (i < heights.length) {
+            if (stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+                stack.push(i);
+                i++;
+            } else {
+                int top = stack.pop();
+                int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        int maxArea = largestRectangleArea(heights);
+        System.out.println("The area of the largest rectangle in the histogram is: " + maxArea); // Output: 10
+    }
+}
