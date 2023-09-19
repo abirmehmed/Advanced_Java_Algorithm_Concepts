b82f9053a4ff16999fb74ebcb3c739da2c0ef644
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-09-19 23:30:14 +0600

    Create SelectionSort.java

---
+++ 02_Sorting_Algorithms/02_Selection Sort:/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6};
+        selectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
