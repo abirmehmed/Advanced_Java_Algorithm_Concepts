f3131190ad52a0f2d1dab081398e36a94ab8ce9d
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-09-21 17:29:27 +0600

    Update CountingSort.java

--- 02_Sorting_Algorithms/06_Counting Sort/CountingSort.java
+++ 02_Sorting_Algorithms/06_Counting Sort/CountingSort.java
@@ -1,1 +1,38 @@
 
+public class CountingSort {
+    public static void countingSort(int[] arr) {
+        int max = getMaxValue(arr);
+        int[] count = new int[max + 1];
+
+        for (int num : arr) {
+            count[num]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i <= max; i++) {
+            while (count[i] > 0) {
+                arr[index++] = i;
+                count[i]--;
+            }
+        }
+    }
+
+    public static int getMaxValue(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6};
+        countingSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
