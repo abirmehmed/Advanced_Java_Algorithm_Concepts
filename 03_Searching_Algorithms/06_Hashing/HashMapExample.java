f843a4e7af8bd1c966429b15b6db89cfd1cae05a
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-04 23:00:55 +0600

    Update HashMapExample.java

--- 03_Searching_Algorithms/06_Hashing/HashMapExample.java
+++ 03_Searching_Algorithms/06_Hashing/HashMapExample.java
@@ -1,1 +1,22 @@
+import java.util.HashMap;
 
+public class HashMapExample {
+    public static void main(String[] args) {
+        // Create a new HashMap
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // Add key-value pairs to the HashMap
+        map.put("John", 25);
+        map.put("Alice", 30);
+        map.put("Bob", 35);
+        map.put("Jane", 40);
+
+        // Retrieve values from the HashMap using keys
+        int johnAge = map.get("John");
+        int aliceAge = map.get("Alice");
+
+        // Print the retrieved values
+        System.out.println("John's age: " + johnAge);
+        System.out.println("Alice's age: " + aliceAge);
+    }
+}
