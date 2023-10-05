14df89ab95ebc59378dab2523cade45567bc3769
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-05 23:15:39 +0600

    Update DepthFirstSearch.java

--- 03_Searching_Algorithms/08_Depth-First Search (DFS)/DepthFirstSearch.java
+++ 03_Searching_Algorithms/08_Depth-First Search (DFS)/DepthFirstSearch.java
@@ -1,1 +1,62 @@
+import java.util.Stack;
 
+class Node {
+    int value;
+    Node left, right;
+
+    Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
+
+public class DepthFirstSearch {
+    Node root;
+
+    boolean DFS(int target) {
+        if (root == null) {
+            return false;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            System.out.println("Visited node: " + current.value);
+
+            if (current.value == target) {
+                return true;
+            }
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DepthFirstSearch tree = new DepthFirstSearch();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+        int target = 5;
+
+        if (tree.DFS(target)) {
+            System.out.println("Target element " + target + " found!");
+        } else {
+            System.out.println("Target element " + target + " not found!");
+        }
+    }
+}
