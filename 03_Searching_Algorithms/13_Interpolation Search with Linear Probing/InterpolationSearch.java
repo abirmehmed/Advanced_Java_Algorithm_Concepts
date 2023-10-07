9c44625cdec9f863f9a79053e8055b8dde70f95a
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-07 22:53:21 +0600

    Create InterpolationSearch.java

---
+++ 03_Searching_Algorithms/13_Interpolation Search with Linear Probing/InterpolationSearch.java
@@ -0,0 +1,42 @@
+public class InterpolationSearch {
+    public static int interpolationSearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high && target >= arr[low] && target <= arr[high]) {
+            if (low == high) {
+                if (arr[low] == target) {
+                    return low;
+                }
+                return -1; // Target not found
+            }
+
+            int pos = low + ((target - arr[low]) * (high - low)) / (arr[high] - arr[low]);
+
+            if (arr[pos] == target) {
+                return pos;
+            }
+
+            if (arr[pos] < target) {
+                low = pos + 1;
+            } else {
+                high = pos - 1;
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60};
+        int target = 40;
+
+        int result = interpolationSearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
