27db378d5874a6ba44ec917d17074836d45ea5bf
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-17 12:48:18 +0600

    Update Factorial.java

--- 04_Recursion/01_Factorial/Factorial.java
+++ 04_Recursion/01_Factorial/Factorial.java
@@ -1,1 +1,16 @@
+public class Factorial {
+    public static int factorial(int n) {
+        // base case: factorial of 0 is 1
+        if (n == 0) {
+            return 1;
+        }
+        // recursive case: factorial of n is n multiplied by factorial of (n-1)
+        return n * factorial(n - 1);
+    }
 
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
