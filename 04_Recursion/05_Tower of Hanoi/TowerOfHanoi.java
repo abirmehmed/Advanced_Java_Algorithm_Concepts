c5c7e667af8807db8d3578c5a675f160e6ec45b7
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-21 12:27:45 +0600

    Update TowerOfHanoi.java

--- 04_Recursion/05_Tower of Hanoi/TowerOfHanoi.java
+++ 04_Recursion/05_Tower of Hanoi/TowerOfHanoi.java
@@ -1,1 +1,27 @@
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, char source, char destination, char auxiliary) {
+        // base case: if there is only one disk, move it from source to destination
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+        
+        // move n-1 disks from source to auxiliary using destination as the auxiliary peg
+        towerOfHanoi(n - 1, source, auxiliary, destination);
+        
+        // move the nth disk from source to destination
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        
+        // move the n-1 disks from auxiliary to destination using source as the auxiliary peg
+        towerOfHanoi(n - 1, auxiliary, destination, source);
+    }
 
+    public static void main(String[] args) {
+        int numberOfDisks = 3;
+        char sourcePeg = 'A';
+        char destinationPeg = 'C';
+        char auxiliaryPeg = 'B';
+        
+        towerOfHanoi(numberOfDisks, sourcePeg, destinationPeg, auxiliaryPeg);
+    }
+}
