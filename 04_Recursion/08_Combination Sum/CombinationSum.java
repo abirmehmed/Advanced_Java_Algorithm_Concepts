cfba10be777c509915411c775eaefb75e139e808
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-23 19:02:26 +0600

    Update CombinationSum.java

--- 04_Recursion/08_Combination Sum/CombinationSum.java
+++ 04_Recursion/08_Combination Sum/CombinationSum.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class CombinationSum {
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        combinationSumHelper(candidates, target, 0, new ArrayList<>(), combinations);
+        return combinations;
+    }
+
+    private static void combinationSumHelper(int[] candidates, int target, int startIndex, List<Integer> currentCombination, List<List<Integer>> combinations) {
+        if (target == 0) {
+            combinations.add(new ArrayList<>(currentCombination));
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                currentCombination.add(candidates[i]);
+                combinationSumHelper(candidates, target - candidates[i], i, currentCombination, combinations);
+                currentCombination.remove(currentCombination.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
+        List<List<Integer>> combinations = combinationSum(candidates, target);
+
+        System.out.println("Combinations that sum up to " + target + ":");
+        for (List<Integer> combination : combinations) {
+            System.out.println(combination);
+        }
+    }
+}
