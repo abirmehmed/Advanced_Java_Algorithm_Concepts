aa084d44fd4c44efdcb689b9ee5410107c053a5a
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-27 22:28:55 +0600

    Update PowerFunction.java

--- 04_Recursion/11_Power Function/PowerFunction.java
+++ 04_Recursion/11_Power Function/PowerFunction.java
@@ -1,1 +1,18 @@
+public class PowerFunction {
+    public static double power(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent > 0) {
+            return base * power(base, exponent - 1);
+        } else {
+            return 1 / (base * power(base, -exponent));
+        }
+    }
 
+    public static void main(String[] args) {
+        double base = 2;
+        int exponent = 3;
+        double result = power(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
