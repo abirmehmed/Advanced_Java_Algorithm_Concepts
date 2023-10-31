4e462320c6ca640557a187fdebe3b710456b30cc
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-10-31 23:29:50 +0600

    Update StringReversal.java

--- 04_Recursion/12_String Reversal/StringReversal.java
+++ 04_Recursion/12_String Reversal/StringReversal.java
@@ -1,1 +1,16 @@
+public class StringReversal {
+    public static String reverseString(String str) {
+        if (str.isEmpty()) {
+            return str;
+        } else {
+            return reverseString(str.substring(1)) + str.charAt(0);
+        }
+    }
 
+    public static void main(String[] args) {
+        String input = "Hello, World!";
+        String reversed = reverseString(input);
+        System.out.println("Original string: " + input);
+        System.out.println("Reversed string: " + reversed);
+    }
+}
