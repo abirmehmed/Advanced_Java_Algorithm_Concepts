4c94277f94a9a7f99ded248130015f206fc412ff
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-11-03 00:26:41 +0600

    Update SubsetGeneration.java

--- 04_Recursion/13_Subset Generation/SubsetGeneration.java
+++ 04_Recursion/13_Subset Generation/SubsetGeneration.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class SubsetGeneration {
+    public static List<List<Integer>> generateSubsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+        return subsets;
+    }
+
+    private static void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> subsets) {
+        subsets.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(nums, i + 1, current, subsets);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> subsets = generateSubsets(nums);
+        System.out.println("Subsets of the given set: ");
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+}
