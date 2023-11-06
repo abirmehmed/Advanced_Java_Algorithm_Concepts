aafb4a03bfd0d4397c8558a50ba2f86e9b451a67
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-11-06 23:28:20 +0600

    Update BinaryTreeHeigh.java

--- 04_Recursion/14_Tree Heigh/BinaryTreeHeigh.java
+++ 04_Recursion/14_Tree Heigh/BinaryTreeHeigh.java
@@ -1,1 +1,35 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
 
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTreeHeight {
+    public static int calculateHeight(Node root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = calculateHeight(root.left);
+            int rightHeight = calculateHeight(root.right);
+
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        int height = calculateHeight(root);
+        System.out.println("Height of the binary tree is: " + height);
+    }
+}
